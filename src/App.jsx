283b1c591e44923ac5ce914b98a8a857abfec3ed
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-11-05 14:03:30 +0900

    パス変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
-import { Login } from "./pages/Login"
+import { Login } from './pages/login'
 
 
 function App() {
