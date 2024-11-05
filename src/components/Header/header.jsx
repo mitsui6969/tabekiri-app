38d6d981e3eb4733f4e87533e90a36db3f2af973
nagisa <c1318728@st.kanazawa-it.ac.jp>
2024-11-05 17:16:24 +0900

    メニュー変更

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -9,21 +9,25 @@ export const Header = () => {
                 <Link to='/' className="logo">LOGO</Link>
                 </div>
 
-                <div>
-                <nav className="navigates">
-                    <ul>
-                        <li>
-                            <Link to={'/'} className="link">Home</Link>
-                        </li>
-                        <li>
-                            <Link to={'/login'} className="link">Login</Link>
-                        </li>
-                        <li>
-                            <Link to={'/bluebutton'} className="link">BlueBotton</Link>
-                        </li>
-                    </ul>
-                </nav>
-                </div>
+                <details>
+                    <summary className="menue">menue</summary>
+                    <nav className="navigates">
+                        <ul>
+                            <li>
+                                <Link to={'/'} className="link">Home</Link>
+                            </li>
+                            <li>
+                                <Link to={'/login'} className="link">Login</Link>
+                            </li>
+                            <li>
+                                <Link to={'/bluebutton'} className="link">BlueBotton</Link>
+                            </li>
+                            <li>
+                                <Link to={'/map'} className='link'>map</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </details>
             </header>
         </div>
     )
