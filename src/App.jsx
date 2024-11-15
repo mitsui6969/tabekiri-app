08acaaa2fe9d8c0378fc4a77062df395a89d91ab
nagisa <136070735+mitsui6969@users.noreply.github.com>
2024-11-15 14:29:40 +0900

    不要なインポート削除

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import './App.css'
-import { Header } from './components/Header/header'
 import { Home } from "./pages/Home"
 import { LoginOrSignup } from './pages/LoginOrSignup'
 import { Login } from './pages/login'
 import { Signup } from './pages/Signup'
-import { BlueBotton } from './pages/BlueBotton'
-
+import { Header } from './components/Header/header'
 import { Footer } from './components/footer/footer'
-
 import {QrScanner}  from './pages/QrScanner'
 
 
@@ -21,17 +18,13 @@ function App() {
 
     <Router>
       <Header/>
-
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/LoginOrSignup" element={<LoginOrSignup/>} />
         <Route path="/Signup" element={<Signup/>} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/bluebutton" element={<BlueBotton/>} />
         <Route path="/QRcode" element={<QrScanner/>} />
-
       </Routes>
-
       <Footer/>
     </Router>
   )
