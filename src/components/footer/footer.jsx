e2347b8cd20ecd96ee2bcd15a0a9b4a6a3b4a89a
nagisa <136070735+mitsui6969@users.noreply.github.com>
2024-11-15 17:27:53 +0900

    修正

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,14 +3,11 @@ import './footer.css'
 import { Link } from 'react-router-dom';
 
 export const Footer = () => {
-    const handleClick = () => {
-        alert("QRコードボタンがクリックされました！");
-    };
     
     return(
         <div className="background-bar">
             <Link to='/QRcode'>
-                <div className="circle-button" onClick={handleClick}>
+                <div className="circle-button">
                     <img src="https://via.placeholder.com/60x60.png?text=QR" alt="QR Code" />
                 </div>
             </Link>
