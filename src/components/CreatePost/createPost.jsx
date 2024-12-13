4ece290e111faabf88aa34fe0f502efda310cb01
nagisa <136070735+mitsui6969@users.noreply.github.com>
2024-12-13 15:59:15 +0900

    テキストフィールド作成

--- src/components/CreatePost/createPost.jsx
+++ src/components/CreatePost/createPost.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './createPost.css'
+import { TextField } from "@mui/material";
 
 const CreatePost = ({handleShowModal}) => {
+    const [TextPost, setTextPost] = ('');
+
     const handleCloseModal = () => {
         handleShowModal(false);
     }
+
+    const onChangeInput = (event) => {      //入力内容が変化したときに実行する関数
+        setTextPost(event.target.value); 
+    };
 
     return (
         <>
@@ -16,7 +23,14 @@ const CreatePost = ({handleShowModal}) => {
                 </div>
                 <div className="modalCenter">
                     {/* ここに書く */}
-                    <p>最近どう？</p>
+                    <TextField 
+                        sx={{ "& .MuiInputBase-input": { height: 50 }, width: 500 }}
+                        label="最近どう？"
+                        multiline
+                        rows={10} // 高さを行で設定
+                        variant="standard"
+                        onChange={onChangeInput} 
+                    /> 
                 </div>
                 <div className="modalBottom">
                     {/* ここに画像選択とか、文字数とか */}
